6f68ae44fc52e872fbd6ffb1c4aa737545a6d8b1
Taseem Molique <taseenm100@gmail.com>
2024-10-16 14:43:23 -0400

    reportManagement and Trasaction

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class Transaction {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private String amount;
+
+    public Transaction (String date, String time, String description, String vendor, String amount){
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String toCSV() {
+        return String.format("%s|%s|%s|%s|%s", date, time, description, vendor, amount);
+
+    }
+
+}
---
+++ src/main/java/com/pluralsight/reportManagement.java
@@ -0,0 +1,120 @@
+package com.pluralsight;
+
+import javax.xml.catalog.Catalog;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class reportManagement {
+    private static final String dataFileName = "transactions.csv";
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void displayReports() {
+        while (true) {
+            System.out.println("\n----- Reports -----");
+            System.out.println("1) Month to Date");
+            System.out.println("2) Previous Month");
+            System.out.println("3) Year to date");
+            System.out.println("4) Previous Year");
+            System.out.println("5) Search by Vendor");
+            System.out.println("0) Back");
+            System.out.println("Please Select an option: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    filterByDate(getCurrentMonthStart(), new Date());
+                    break;
+                case "2":
+                    filterByDate(getPreviousMonthStart(), getPreviousMonthEnd());
+                    break;
+                case "3":
+                    filterByDate(getCurrentYearStart(), new Date());
+                    break;
+                case "4":
+                    filterByDate(getPreviousYearStart(), getPreviousYearEnd());
+                case "5":
+                    System.out.println("Enter vendor name: ");
+                    String vendor = scanner.nextLine();
+                    filterByVendor(vendor);
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+
+            }
+
+
+        }
+    }
+
+    private static void filterByDate(Date startDate, Date endDate) {
+        System.out.println("\n---- Filtered by Date ----");
+        SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
+        try (BufferedReader BR = new BufferedReader(new FileReader(dataFileName))) {
+            String line;
+            while ((line = BR.readLine()) != null) {
+                String[] tokens = line.split("\\|");
+                Date transactionDate = SDF.parse(tokens[0]);
+                if ((transactionDate.equals(startDate) || transactionDate.after(startDate)) &&
+                        (transactionDate.equals(endDate) || transactionDate.before(endDate))) {
+                    System.out.println(line.replace('|', ' '));
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error reading ledger: " + e.getMessage());
+        }
+
+    }
+
+    private static void filterByVendor(String vendor) {
+        System.out.println("\n---- Filtered by Vendor: " + vendor + " ----");
+        try (BufferedReader br = new BufferedReader(new FileReader(dataFileName))){
+            String line;
+            while ((line = br.readLine()) != null){
+                String [] tokens = line.split("\\|");
+                if (tokens[3].equalsIgnoreCase(vendor)){
+                    System.out.println(line.replace('|', ' '));
+                }
+            }
+        } catch (Exception e){
+            System.out.println("Error reading ledger: "+ e.getMessage());
+        }
+
+    }
+
+    private static Date getCurrentMonthStart() {
+        return new Date();
+    }
+
+    private static Date getPreviousMonthStart() {
+        return new Date();
+    }
+
+    private static Date getPreviousMonthEnd() {
+        return new Date();
+    }
+
+    private static Date getCurrentYearStart() {
+        return new Date();
+    }
+
+    private static Date getPreviousYearStart() {
+        return new Date();
+    }
+
+    private static Date getPreviousYearEnd() {
+        return new Date();
+    }
+
+}
+
+
